fd3d84ecd3ffb405ed5a336293396be084c2183b
Aditya Bhaskar <91673723+utdady@users.noreply.github.com>
2023-09-25 14:43:16 -0700

    Add files via upload

---
+++ Python 2020/hello world/helloo.py
@@ -0,0 +1,5 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> 
