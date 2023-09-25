b1cfa4051f45f2047957389139c9e198d8614a62
Aditya Bhaskar <91673723+utdady@users.noreply.github.com>
2023-09-25 14:39:01 -0700

    Delete Python 2020/hello world directory

--- Python 2020/hello world/test1.py
+++
@@ -1,8 +0,0 @@
-Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a=2
->>> b=3
->>> c=a*b
->>> print (c)
-6
->>> 
